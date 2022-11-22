750e583158c0fcf2e3ad8ae2d8931c5205c7c7ce
«Roman <research777@mail.ru>
2022-11-22 16:08:54 +0300

    SEMINAR4_Ex1_v.1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+
+//1) void Met1(){} - невозвратный метод (считает, показывает, не возвращает)
+
+
+//2) int / double / bool /  Met1(){...return args;} - возвратный метод (может вернуть результат или др. данные из метода)
+
+// 1)
+
+void MetFindSum(int a)
+{
+    int sum = 0;      // переменная, которая в последствии будет накапливать результат
+    for (int current = 1; current <= a; current++)  // current -переменная условия цикла(для цикла), =1 -по условию отрезка, <= a -по условию отрезка, ++ -каждый цикл +1.
+    {
+        sum += current;  // -сокращенная запись для summa = summa + current
+    }
+    Console.WriteLine ($"Sum of elements from 1 to {a} is {sum} ");
+}
+
+Console.WriteLine("Input your number: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+MetFindSum(userNum);
+
+//В этом методе невозможно в дальнейшем взаимодействие с результатом (только проверка результата)
+
+// 2)
